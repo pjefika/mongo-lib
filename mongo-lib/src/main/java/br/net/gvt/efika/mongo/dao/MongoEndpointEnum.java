7139d3278b9b9952c65446a38d7686df25bf2181
Henrique Merlin <henmerlin@gmail.com>
2018-03-06 09:49:54 -0300

    + MongoEndpointEnum

---
+++ mongo-lib/src/main/java/br/net/gvt/efika/mongo/dao/MongoEndpointEnum.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.net.gvt.efika.mongo.dao;
+
+/**
+ *
+ * @author G0042204
+ */
+public enum MongoEndpointEnum {
+
+    MONGO("10.200.35.66");
+//    MONGO("10.40.197.137");
+
+    private final String ip;
+
+    private MongoEndpointEnum(String ip) {
+        this.ip = ip;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+}
